099c18144d13db7e914e6c1f5f6cc41e014e6e1d
Tomus_kd35 <Tomus_kd35@student.itstep.org>
2024-04-16 20:23:07 +0300

    Add project files.

---
+++ Task1.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ConsoleApp15
+{
+    internal class Task1
+    {
+        public void Task(int size, char s)
+        {
+            for(int i = 0; i < size; i++) {
+                for(int j = 0; j < size; j++)
+                {
+                    Console.Write(s);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
